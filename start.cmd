848c58e98b1ac33369544fcb3dec2f104ea5c5c7
Random568 <50462383+Random568@users.noreply.github.com>
2019-07-22 07:53:34 +0200

    Create start.cmd

---
+++ start.cmd
@@ -0,0 +1,27 @@
+@echo off
+REM This code is used to start a server running with a SteadFast2.phar
+TITLE PocketMine-MP server software for Minecraft: Pocket Edition
+cd /d %~dp0
+
+if exist bin\php\php.exe (
+	set PHPRC=""
+	set PHP_BINARY=bin\php\php.exe
+) else (
+	set PHP_BINARY=php
+)
+
+if exist SteadFast2.phar (
+	set POCKETMINE_FILE=SteadFast2.phar
+) else (
+	echo SteadFast2 not found
+	echo Downloads can be found at https://github.com/Hydreon/Steadfast2
+	pause
+	exit 1
+)
+
+if exist bin\mintty.exe (
+	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-MP" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+) else (
+	REM pause on exitcode != 0 so the user can see what went wrong
+	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %* || pause
+)
